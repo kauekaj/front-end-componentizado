ced57d3dffd9e5614cf193fa3b0fe4ba67718822
Kauê Jacyntho <kauekaj@gmail.com>
2021-11-23 20:02:11 -0300

    fixing bug

--- github-api/src/App.js
+++ github-api/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import Layout from './components/layout';
 import Profile from './components/profile';
 
@@ -6,28 +6,9 @@ const App = () => {
   return (
     <main>
       <Layout>
-        <div>
-        <img 
-                src="https://avatars.githubusercontent.com/u/72616256?v=4"
-                alt="Avatar of user"
-            />
-                <h1>Kauê Jacyntho</h1>
-                <h3>Username: </h3>
-                <span>kauekaj</span>
-                <div>
-                    <h4>Followers</h4>
-                    <span>5</span>  
-                </div>
-                <div>
-                    <h4>Starreds</h4>
-                    <span>5</span>  
-                </div>
-                <div>
-                    <h4>Followings</h4>
-                    <span>5</span>  
-                </div>  
-        </div>
-                   
+        
+        <Profile />
+        
         <div>Repositories</div>
         <div>Starreds</div>
       </Layout>  
