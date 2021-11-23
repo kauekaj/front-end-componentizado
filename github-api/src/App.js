00dcc1d90adf38e20c27c899830eff2ef7ca45d4
Kauê Jacyntho <kauekaj@gmail.com>
2021-11-23 18:11:47 -0300

    Update App.js

--- github-api/src/App.js
+++ github-api/src/App.js
@@ -6,7 +6,27 @@ const App = () => {
   return (
     <main>
       <Layout>
-        <Profile />
+        <div>
+        <img 
+                src="https://avatars.githubusercontent.com/u/72616256?v=4"
+                alt="Avatar of user"
+            />
+                <h1>Kauê Jacyntho</h1>
+                <h3>Username: </h3>
+                <span>kauekaj</span>
+                <div>
+                    <h4>Followers</h4>
+                    <span>5</span>  
+                </div>
+                <div>
+                    <h4>Starreds</h4>
+                    <span>5</span>  
+                </div>
+                <div>
+                    <h4>Followings</h4>
+                    <span>5</span>  
+                </div>  
+        </div>
                    
         <div>Repositories</div>
         <div>Starreds</div>
