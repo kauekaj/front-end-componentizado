f81c6fec929833bad9f7bf7688428264f0793218
Kauê Jacyntho <kauekaj@gmail.com>
2021-11-24 13:48:06 -0300

    fixing styled-components error

--- github-api/src/App.js
+++ github-api/src/App.js
@@ -7,28 +7,7 @@ const App = () => {
     <main>
       <Layout>
         
-      <div>
-                 <img 
-                    src="https://avatars.githubusercontent.com/u/72616256?v=4"
-                    alt="Avatar of user"
-                 />
-                    <h1>Kauê Jacyntho</h1>
-                    <h3>Username: </h3>
-                    <span>kauekaj</span>
-                    <div>
-                        <h4>Followers</h4>
-                        <span>5</span>  
-                    </div>
-                    <div>
-                        <h4>Starreds</h4>
-                        <span>5</span>  
-                    </div>
-                    <div>
-                        <h4>Followings</h4>
-                        <span>5</span>  
-                    </div>
-                </div>    
-        
+        <Profile />
         <div>Repositories</div>
         <div>Starreds</div>
       </Layout>  
--- github-api/src/components/profile/index.js
+++ github-api/src/components/profile/index.js
@@ -4,27 +4,27 @@ import * as S from "./styled";
 const Profile = () => {
     return <S.Wrapper>
               
-              <div>
-                 <img 
+            <div>
+                <img 
                     src="https://avatars.githubusercontent.com/u/72616256?v=4"
                     alt="Avatar of user"
-                 />
-                    <h1>Kauê Jacyntho</h1>
-                    <h3>Username: </h3>
-                    <span>kauekaj</span>
-                    <div>
-                        <h4>Followers</h4>
-                        <span>5</span>  
-                    </div>
-                    <div>
-                        <h4>Starreds</h4>
-                        <span>5</span>  
-                    </div>
-                    <div>
-                        <h4>Followings</h4>
-                        <span>5</span>  
-                    </div>
-                </div>    
+                />
+                <h1>Kauê Jacyntho</h1>
+                <h3>Username: </h3>
+                <span>kauekaj</span>
+                <div>
+                    <h4>Followers</h4>
+                    <span>5</span>  
+                </div>
+                <div>
+                    <h4>Starreds</h4>
+                    <span>5</span>  
+                </div>
+                <div>
+                    <h4>Followings</h4>
+                    <span>5</span>  
+                </div>
+            </div>    
                            
             </S.Wrapper>;
 };
