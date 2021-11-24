d4861c5ebe2a17c5664069c8423f542124c66de5
Kauê Jacyntho <kauekaj@gmail.com>
2021-11-24 18:11:18 -0300

    create resetCSS

--- github-api/src/components/profile/styled.js
+++ github-api/src/components/profile/styled.js
@@ -13,9 +13,22 @@ display: flex;
 flex-direction: column;
 align-items: flex-start;
 justify-content: space-between;
-height: auto;
+height: 200px;
 margin-left: 8px;
+h1 {
+    font-size: 32px;
+    font-weight: bold;
+}
 
+h3 {
+    font-size: 18px;
+    font-weight: bold;
+}
+
+h4 {
+    font-size: 16px;
+    font-weight: bold;
+}
 `;
 
 export const WrapperStatusCount = styled.div`    
@@ -26,17 +39,23 @@ div {
     margin: 8px;
     text-align: center;
 }
-
 `;
 
 export const WrapperUsername = styled.div`    
 
 display: flex;
 align-items: center;
+margin-top 8px;
+
 h3 {
     margin-right: 8px;
 }
-
+a {
+    font-size: 18px;
+    color: blue;
+    font-weight: bold;
+    
+}
 `;
 
 export const WrapperImage = styled.img`    
@@ -44,5 +63,4 @@ export const WrapperImage = styled.img`
 border-radius: 50%;
 width: 200px;
 margin: 8px;
-
 `;
