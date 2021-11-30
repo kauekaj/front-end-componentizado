3b67ab58964b57b8f1a7690c99f25503466fd765
Kauê Jacyntho <kauekaj@gmail.com>
2021-11-30 16:33:21 -0300

    update repository-item

--- github-api/src/components/repositories/index.js
+++ github-api/src/components/repositories/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import RepositoryItem from "../repository-item";
 import * as S from "./styled";
 
 const Repositories = () => {
@@ -11,8 +12,20 @@ const Repositories = () => {
                   <S.WrapperTab>Repositories</S.WrapperTab>
                   <S.WrapperTab>Starred</S.WrapperTab>
               </S.WrapperTabList>
-              <S.WrapperTabPanel>panel Repositories</S.WrapperTabPanel>    
-              <S.WrapperTabPanel>panel Starred</S.WrapperTabPanel>    
+              <S.WrapperTabPanel>
+                <RepositoryItem
+                    name= "kauekaj"
+                    linkToRepo= "https://github.com/kauekaj/kauekaj"
+                    fullName= "kauekaj/kauekaj"
+                />
+              </S.WrapperTabPanel>    
+              <S.WrapperTabPanel>
+                <RepositoryItem      
+                    name= "projeto-site"
+                    linkToRepo= "https://github.com/kauekaj/projeto-site"
+                    fullName= "kauekaj/projeto-site"
+                />    
+              </S.WrapperTabPanel>    
         </S.WrapperTabs>
     );
 };
--- github-api/src/components/repository-item/index.js
+++ github-api/src/components/repository-item/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 
-const RepositoryItem = () => {
+const RepositoryItem = ({name, linkToRepo, fullName}) => {
     return <div>
-        
+      <h2>{ name }</h2>
+      <h4>full name</h4>
+      <a href={ linkToRepo }
+                   target="_blank"
+                   rel="noreferrer"
+       > { fullName }
+       </a>  
     </div>;
 };
 
