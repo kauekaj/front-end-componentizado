12264763cf9523c5487be06a32c89408bb3af4d6
Kauê Jacyntho <kauekaj@gmail.com>
2021-11-30 18:31:08 -0300

    create header.js

--- github-api/src/components/profile/index.js
+++ github-api/src/components/profile/index.js
@@ -16,26 +16,30 @@ const Profile = () => {
                 <h1>{githubState.user.name}</h1>
                 <S.WrapperUsername>
                 <h3>Username: </h3>
-                <a href="https://github.com/kauekaj"
+                <a href={githubState.user.html_url}
                    target="_blank"
                    rel="noreferrer"
                 >
-                    kauekaj
+                    {githubState.user.login}
                 </a>
                 </S.WrapperUsername>
                 </div>
                 <S.WrapperStatusCount>
                     <div>
                         <h4>Followers</h4>
-                        <span>5</span>  
-                    </div>
-                    <div>
-                        <h4>Starred</h4>
-                        <span>5</span>  
+                        <span>{githubState.user.followers}</span>  
                     </div>
                     <div>
                         <h4>Followings</h4>
-                        <span>5</span>  
+                        <span>{githubState.user.following}</span>  
+                    </div>
+                    <div>
+                        <h4>Gists</h4>
+                        <span>{githubState.user.public_gists}</span>  
+                    </div>
+                    <div>
+                        <h4>Repos</h4>
+                        <span>{githubState.user.public_repos}</span>  
                     </div>
                 </S.WrapperStatusCount>
               </S.WrapperInfoUser>
