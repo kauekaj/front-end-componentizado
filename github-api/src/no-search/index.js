9cc38312e2d706968df45a43673062af6837fcb9
Kauê Jacyntho <kauekaj@gmail.com>
2021-12-02 16:23:15 -0300

    update no-search

--- github-api/src/no-search/index.js
+++ github-api/src/no-search/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import * as S from "./styled";
 
 const NoSearch = () => {
-    return <S.Wrapper>Nenhum usuário pesquisado</S.Wrapper>;
+    return <S.Wrapper>
+               <h1>Nenhum usuário pesquisado</h1>
+            </S.Wrapper>;
 };
 
 export default NoSearch;
