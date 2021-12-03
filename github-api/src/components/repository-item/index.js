3ffb5ddf25b73c92788ae07450977f951140b8b1
Kauê Jacyntho <kauekaj@gmail.com>
2021-12-03 17:07:02 -0300

    update repository-item

--- github-api/src/components/repository-item/index.js
+++ github-api/src/components/repository-item/index.js
@@ -1,15 +1,17 @@
 import React from "react";
+import * as S from "./styled"
 
 const RepositoryItem = ({name, linkToRepo, fullName}) => {
-    return <div>
-      <h2>{ name }</h2>
-      <h4>full name</h4>
-      <a href={ linkToRepo }
+    return <S.Wrapper>
+      <S.WrapperTitle>{ name }</S.WrapperTitle>
+      <br></br>
+      <S.WrapperFullName> full name:</S.WrapperFullName>
+      <S.WrapperLink href={ linkToRepo }
                    target="_blank"
                    rel="noreferrer"
        > { fullName }
-       </a>  
-    </div>;
+       </S.WrapperLink>  
+    </S.Wrapper>;
 };
 
 export default RepositoryItem;
---
+++ github-api/src/components/repository-item/styled.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+border: 1px solid #ccc;
+box-shadow: 2px 4px #ccc;
+border-radius: 8px;
+margin: 16px 16px;
+padding: 2px 5px;
+width: 320px;
+height: 120px;
+align-content: center;
+`;
+
+export const WrapperTitle = styled.h2`
+font-size: 18px;
+font-weight: bold;
+margin: 8px 0;
+`;
+
+export const WrapperFullName = styled.h4`
+font-size: 14px;
+font-weight: bold;
+margin: 8px 0;
+color: #2d3741;
+`;
+
+export const WrapperLink = styled.a`
+font-size: 14px;
+font-weight: bold;
+margin: 8px 0;
+color: #3182ce;
+`;
